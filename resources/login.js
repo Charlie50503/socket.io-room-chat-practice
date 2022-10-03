633c3d8bfca5e67e57cb0ff129a879f131df701b
charlie50503 <cate50503@gmail.com>
2022-10-03 13:25:43 +0900

    feat: 修改login layout並添加toast

--- resources/login.js
+++ resources/login.js
@@ -1,10 +1,38 @@
 const loginUserInput = document.getElementById("LOGIN_USER_INPUT")
+const roomNameInput = document.getElementById("ROOM_NAME_INPUT")
+
+var alertToast = null
+
 function login(){
-  if(!isInputHasValue(loginUserInput)){
-    alert("請輸入user name")
+  if(!isInputHasValue(roomNameInput)){
+    showAlert("請輸入 Room name")
     return
   }
 
+  if(!isInputHasValue(loginUserInput)){
+    showAlert("請輸入 User name")
+    return
+  }
+  
+
   localStorage.setItem('userName', loginUserInput.value);
   window.location.href = "index.html";
+}
+
+function init(){
+  addEnterEvent()
+  alertToast = createToast('liveToast')
+}
+
+function addEnterEvent(){
+  window.document.onkeydown = function(event){
+    if (event.key === 'Enter') login()
+  }
+}
+
+function showAlert(messageHtml){
+  const alertToastBody = document.getElementById("ALERT_TOAST_BODY")
+  alertToastBody.innerHTML = /*html*/`<p font="noto" class="mb-0">${messageHtml}</p>`
+  alertToast.show();
+  timeToHideToast(2000,alertToast)
 }
